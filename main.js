0ed69da5dbaf0c9a17fd232fe82e3444ca5e3adf
Will Hedges <wshedges@outlook.com>
2022-12-12 09:21:59 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = ["Clay", "Clay", "Clay", "Clay"];
+const toFireInKiln = [];
+
+for (const mound of clay) {
+  const mug = `${mound} coffee mug`;
+  toFireInKiln.push(mug);
+}
+
+console.log(toFireInKiln);
